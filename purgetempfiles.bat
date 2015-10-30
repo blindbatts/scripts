5920ebeb89e9f53ca89e4fc01020c975dfe40f54
blindbatts <blindbatts@gmail.com>
2015-10-30 10:04:46 -0700

    Create purgetempfiles.bat

---
+++ purgetempfiles.bat
@@ -0,0 +1,9 @@
+@echo off   
+cd /d %temp%   
+for /r %%a in (*.*) do (   
+del /f /q %~a   
+) >nul   
+cd /d "%UserProfile%\Local Settings\Temporary Internet Files   
+for /r %%b in (*.*) do (   
+del /f /q %~b   
+) >nul   
