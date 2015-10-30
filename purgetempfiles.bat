56c6e93190762fe746b0d626fab93bc98fd52407
blindbatts <blindbatts@gmail.com>
2015-10-30 10:30:51 -0700

    Update purgetempfiles.bat

--- purgetempfiles.bat
+++ purgetempfiles.bat
@@ -1,9 +1,9 @@
 @echo off   
 cd /d %temp%   
 for /r %%a in (*.*) do (   
-del /f /q %~a   
+del /f /q %%a   
 ) >nul   
 cd /d "%UserProfile%\Local Settings\Temporary Internet Files   
 for /r %%b in (*.*) do (   
-del /f /q %~b   
+del /f /q %%b   
 ) >nul   
