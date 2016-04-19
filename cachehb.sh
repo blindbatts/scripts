2de9b4c2fb884a7191a57a7de3388045e4b6add4
blindbatts <blindbatts@gmail.com>
2016-04-19 10:22:34 -0700

    Update cachehb.sh
    added email functionality for notifications

--- cachehb.sh
+++ cachehb.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+adminmail=edit@me.com
+subject="UnityCacheServer Restarted!"
+log=/var/log/unitycache.log
 
-pidof  node >/dev/null
+pidof  node6 >/dev/null
 if [[ $? -ne 0 ]] ; then
-        echo "Restarting UnityCacheServer:     $(date)" >> /var/log/unitycache.txt
+        echo "Restarting UnityCacheServer:     $(date)" >> $log
         /cache/CacheServer/start.sh &
 fi
+
+cat $log | mailx -s "$subject" "$adminmail"
+exit 0
+
+
+
